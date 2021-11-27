61767c3cb4d1aba971eb869756a83721b4fe9e55
Yebin Lee <lyb2325@gmail.com>
2021-11-27 23:55:42 +0900

    update fitbit analysis - add brief_data

--- fitbit_analysis.py
+++ fitbit_analysis.py
@@ -7,7 +7,7 @@ Created on Sat Nov 27 15:27:17 2021
 
 import pandas as pd
 import json
- 
+import numpy as np
 
 '''
 메인 함수 : 파일 입출력, data 파일 json 포맷으로 변환
@@ -35,8 +35,10 @@ def main():
 '''    
     
 def all_data(start, end):
+    
     for i in range(start,end):
         # print("<< ", i, ">>" ,end='\n\n')
+    
         
         ex_data=data[f'{i}']
         ex_data = ex_data.replace('\'', '\"')   # json 포맷 변환을 위해 '를 "로 변환
@@ -52,16 +54,39 @@ def all_data(start, end):
     
         # 1분 간격으로 시간당 총 60개의 데이터 출력
         for i in range(len(distance_data)):
+
             print(i)
             print("time : ", distance_data[i]['time'])
             print("distance data : ", distance_data[i]['value'])
             print("calories data : " ,calories_data[i]['level'], calories_data[i]['mets'], calories_data[i]['value'])
             print("steps data : " ,steps_data[i]['value'])
+         
             print("------------------------------\n")
             
-        print("==================================================\n")
-    
+            
 
+        # print("==================================================\n")
+        
+
+def brief_data(start, end):
+    for i in range(start,end):
+        ex_data=data[f'{i}']
+        ex_data = ex_data.replace('\'', '\"')   # json 포맷 변환을 위해 '를 "로 변환
+        jsonData = json.loads(ex_data)          # json 포맷으로 변환
+        
+        date = jsonData['distance']['activities-distance'][0]['dateTime'] # 날짜
+        
+        distance_data = jsonData['distance']['activities-distance-intraday']['dataset']     # 모든 distance 데이터셋
+        calories_data = jsonData['calories']['activities-calories-intraday']['dataset']     # 모든 calories 데이터셋
+        steps_data = jsonData['steps']['activities-steps-intraday']['dataset']              # 모든 steps 데이터셋
+        
+        print("\n=============== ", date, '(', i, ')', ' =============== ', end='\n\n') # 날짜 출력
+    
+        # 1분 간격으로 시간당 총 60개의 데이터 출력
+        for i in range(len(distance_data)):
+            print(distance_data[i]['time'] , ' - ' , distance_data[i]['value'], calories_data[i]['level'], calories_data[i]['mets'], calories_data[i]['value'],steps_data[i]['value'])
+
+        
 
 '''
 raw_data 출력
@@ -108,7 +133,7 @@ def distance_data(start,end):
 
 
 data=main()         # 파일 입출력, 데이터 dictionary를 json 포맷으로 변환하여 얻은 data
-all_data(12,15)     # distance, calories, steps 시간대로 모두 출력
+brief_data(12,38)     # distance, calories, steps 시간대로 모두 출력
 
 
 # raw_data(12,29)
